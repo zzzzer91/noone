72d07e20aaa3a052a0a01d5adc8d471498839257
zzzzer <zzzzer91@gmail.com>
2019-03-11 15:15:10 +0800

    update

--- tests/test_cryptor.c
+++ tests/test_cryptor.c
@@ -12,46 +12,60 @@
 #define KEY_LEN 32
 #define IV_LEN 16
 
-#define PASSWD (unsigned char *)"abc123"
-unsigned char g_key[KEY_LEN+1];
-unsigned char g_iv[IV_LEN+1];
+#define PASSWD "abc123"
+#define PASSWD_TO_KEY "e99a18c428cb38d5f260853678922e0388fc221acae10caf2921f7435051325c"
+#define PASSWD_TO_IV "1243734da46f16a118114ad51cfd48e2"
 
-static void
-hex_print(unsigned char *data, int len)
+static int
+bytes_to_hex(unsigned char *data, int len, char *buf)
 {
-    for (int i = 0; i < len; i++) {
-        printf("%02x", data[i]);
+    int i;
+    for (i = 0; i < len; i++) {
+        sprintf(buf+i*2, "%02x", data[i]);
     }
-    printf("\n");
+    buf[i*2] = '\0';
+
+    return i*2;
 }
 
 static void
 test_bytes_to_key()
 {
-    bytes_to_key(PASSWD, g_key, KEY_LEN, g_iv, IV_LEN);
-    hex_print(g_key, KEY_LEN);
-    hex_print(g_iv, IV_LEN);
+    unsigned char key[KEY_LEN+1];
+    unsigned char iv[IV_LEN+1];
+    bytes_to_key((unsigned char *)PASSWD, key, KEY_LEN, iv, IV_LEN);
+
+    char *key_hex = (char *)malloc((size_t)(KEY_LEN*2+1));
+    int key_hex_len = bytes_to_hex(key, KEY_LEN, key_hex);
+    EXPECT_EQ_STRING(PASSWD_TO_KEY, key_hex, key_hex_len);
+    free(key_hex);
+
+    char *iv_hex = (char *)malloc((size_t)(IV_LEN*2+1));
+    int iv_hex_len = bytes_to_hex(iv, IV_LEN, iv_hex);
+    EXPECT_EQ_STRING(PASSWD_TO_IV, iv_hex, iv_hex_len);
+    free(iv_hex);
 }
 
 static void
 test_encrypt_and_decrypt()
 {
-    bytes_to_key(PASSWD, g_key, KEY_LEN, g_iv, IV_LEN);
-    EVP_CIPHER_CTX *encrypt_ctx = INIT_AES128CTR_ENCRYPT_CTX(g_key, g_iv);
-    EVP_CIPHER_CTX *decrypt_ctx = INIT_AES128CTR_DECRYPT_CTX(g_key, g_iv);
-    unsigned char plaintext[] = "abc123";
+    unsigned char key[KEY_LEN+1];
+    unsigned char iv[IV_LEN+1];
+    bytes_to_key((unsigned char *)PASSWD, key, KEY_LEN, iv, IV_LEN);
+    EVP_CIPHER_CTX *encrypt_ctx = INIT_AES128CTR_ENCRYPT_CTX(key, iv);
+    EVP_CIPHER_CTX *decrypt_ctx = INIT_AES128CTR_DECRYPT_CTX(key, iv);
+    unsigned char plaintext[] = PASSWD;
     int plaintext_len = (int)strlen((char *)plaintext);
     unsigned char ciphertext[256];
     int ciphertext_len = encrypt(encrypt_ctx, plaintext, plaintext_len, ciphertext);
-    // hex_print(ciphertext, ciphertext_len);
     plaintext_len = decrypt(decrypt_ctx, ciphertext, ciphertext_len, plaintext);
     EXPECT_EQ_INT(6, plaintext_len);
-    EXPECT_EQ_STRING("abc123", plaintext, plaintext_len);
+    EXPECT_EQ_STRING(PASSWD, plaintext, plaintext_len);
 }
 
 void
 test_cryptor()
 {
-    // test_bytes_to_key();
+    test_bytes_to_key();
     test_encrypt_and_decrypt();
 }
