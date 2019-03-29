c43e10b5fce5602c7916dda41e6f534291e7357e
zzzzer <zzzzer91@gmail.com>
2019-03-29 20:38:57 +0800

    v0.2

---
+++ src/lru.h
@@ -0,0 +1,12 @@
+/*
+ * Created by zzzzer on 3/29/19.
+ */
+
+#ifndef _NOONE_LRU_H_
+#define _NOONE_LRU_H__
+
+#include <stddef.h>
+
+size_t hash_key(size_t capacity, char *key);
+
+#endif  /* _NOONE_LRU_H_ */
