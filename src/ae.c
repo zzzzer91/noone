4dfc1c1502be141b1a637449e3ce169968b239fb
zzzzer <zzzzer91@gmail.com>
2019-04-15 14:21:31 +0800

    v0.6 test

--- src/ae.c
+++ src/ae.c
@@ -104,14 +104,14 @@ ae_process_events(AeEventLoop *event_loop, int timeout)
         int fd = event_loop->ready_events[i].data.fd;
         AeEvent *fe = &event_loop->events[fd];
         LOGGER_DEBUG("fd: %d, fe->mask: %d, mask: %d", fd, fe->mask, mask);
-        if (fe->mask | mask | EPOLLERR) {
+        if ((fe->mask | EPOLLERR ) & mask & EPOLLERR) {
             LOGGER_DEBUG("fd: %d, EPOLLERR", fd);
             fe->tcallback(event_loop, fd, fe->data);
         }
-        if (fe->mask | mask | (AE_IN|EPOLLHUP)) {
+        if ((fe->mask| EPOLLHUP) & mask & (AE_IN|EPOLLHUP)) {
             fe->rcallback(event_loop, fd, fe->data);
         }
-        if (fe->mask | mask | AE_OUT) {
+        if (fe->mask & mask & AE_OUT) {
             fe->wcallback(event_loop, fd, fe->data);
         }
         processed++;
@@ -244,8 +244,6 @@ ae_register_event(AeEventLoop *event_loop, int fd, uint32_t mask,
     // 取出文件事件结构
     AeEvent *fe = &event_loop->events[fd];
     int fe_mask = fe->mask;
-    fe_mask ^= EPOLLERR;
-    fe_mask ^= EPOLLHUP;
 
     if (fe_mask != mask) {
         // 判断是注册还是修改
@@ -264,7 +262,7 @@ ae_register_event(AeEventLoop *event_loop, int fd, uint32_t mask,
         }
 
         // 设置文件事件类型，以及事件的处理器
-        fe->mask = mask | EPOLLERR | EPOLLHUP;
+        fe->mask = mask;
     }
 
     // 回调
